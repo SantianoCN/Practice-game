a39235dbf6390dcd605b64133b9d78412075c75d
SantianoLaptop <santianogames@gmial.com>
2026-07-08 12:30:32 +0300

    feature: entities and bullets collisions

--- src/managers/CollisionManager.ts
+++ src/managers/CollisionManager.ts
@@ -1,27 +1,66 @@
 import Bullet from "src/entities/Bullet";
 import LivingEntity from "src/entities/LivingEntity";
 
-function collisionDamage(entities: LivingEntity[], bullets: Bullet[]) {
-    for(let i = 0; i < entities.length; i++) {
-        const target = entities[i];
-        for (let j = 0; j < bullets.length; j++) {
-            const bullet = bullets[j];
-            if (bullet.isDestroyed) continue;
-            if (target.hp <= 0) continue;
-            if (bullet.ownerType === target.type) continue; 
+function processCollisions(bullets: Bullet[], players: LivingEntity[], enemies: LivingEntity[],
+    height: number, width: number) {
+    const entities = players.concat(enemies);
 
-            if (bullet.getBounds().top > target.getBounds().top
-                    && bullet.getBounds().top < target.getBounds().bottom) {
-                if (bullet.getBounds().right > target.getBounds().left
-                    && bullet.getBounds().left < target.getBounds().right) {
-                    target.takeDamage(bullet.damage);
-                    bullet.destroy();
-                }
-            }
+    for (let i = 0; i < entities.length; i++) {
+        collisionEntity(entities[i], height, width);
+    }
+
+    for (let j = 0; j < bullets.length; j++) {
+        const bullet = bullets[j];
+        if (bullet.isDestroyed) continue;
+        if (collisionBullet(bullet, height, width)) continue;
+
+        for (let i = 0; i < entities.length; i++) {
+            const entity = entities[i];
+            if (entity.hp <= 0) continue;
+            if (bullet.ownerType === entity.type) continue;
+            checkBulletHit(bullet, entity);
         }
     }
 }
 
-function collisionBorder() {
+function checkBulletHit(bullet: Bullet, target: LivingEntity) {
+    const bulletBounds = bullet.getBounds();
+    const targetBounds = target.getBounds();
+    if (bulletBounds.top > targetBounds.top
+        && bulletBounds.top < targetBounds.bottom) {
+        if (bulletBounds.right > targetBounds.left
+            && bulletBounds.left < targetBounds.right) {
+            target.takeDamage(bullet.damage);
+            bullet.destroy();
+        }
+    }
+}
 
+function collisionBullet(bullet: Bullet, height: number, width: number): boolean {
+    if (bullet.x < 0 || bullet.x > width
+        || bullet.y < 0 || bullet.y > height) {
+        bullet.destroy();
+        return true;
+    }
+    return false;
+}
+
+function collisionEntity(entity: LivingEntity, height: number, width: number) {
+    const entityBound = entity.getBounds();
+    if (entityBound.left < 0) {
+        entity.vx = 0;
+        entity.x = entity.width / 2;
+    }
+    if (entityBound.right > width) {
+        entity.vx = 0;
+        entity.x = width - entity.width / 2;
+    }
+    if (entityBound.top < 0) {
+        entity.vy = 0;
+        entity.y = entity.height / 2;
+    }
+    if (entityBound.bottom > height) {
+        entity.vy = 0;
+        entity.y = height - entity.height / 2;
+    }
 }
