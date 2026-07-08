302c4392c2236621ffea3d780f0c6128b1323fd4
maxifan113 <maxifan113@gmail.com>
2026-07-08 13:56:50 +0300

    Add ReadInput and rewrite NetworcClient on ts

--- client/src/input/readInput.js
+++
@@ -1,8 +0,0 @@
-let keys = {};
-
-window.addEventListener('keydown', (event) => keys[event.key] = true);
-window.addEventListener('keyup', (event) => keys[event.key] = false);
-
-function readKey() {
-  return keys;
-}
