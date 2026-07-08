08aa84138ab867b719554ebb8c17b64bc7b502b5
SantianoLaptop <santianogames@gmial.com>
2026-07-08 16:41:59 +0300

    game manager added

---
+++ src/entities/GameSession.ts
@@ -0,0 +1,11 @@
+import Bullet from "./Bullet";
+import Enemy from "./Enemy";
+import Player from "./Player";
+
+
+export class GameSession {
+    public players: Player[];
+    public enemies: Enemy[];
+    public bullets: Bullet[];
+
+}
