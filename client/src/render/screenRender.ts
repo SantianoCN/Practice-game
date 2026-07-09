2d8a781f9bf8cc5078b96f80f8b90f9841893491
maxifan113 <maxifan113@gmail.com>
2026-07-09 14:10:28 +0300

    Rewrite cordinate logic

--- client/src/render/screenRender.ts
+++ client/src/render/screenRender.ts
@@ -24,13 +24,7 @@ export class GameRender {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
   }
 
-  private drawScreen(snapshot: GameSnapshot): void {
-    this.cameraX = 0;
-    this.cameraY = 0;
-
-    this.context.save;
-    this.context.translate(this.canvas.width / 2, this.canvas.height / 2);
-    
+  private drawScreen(snapshot: GameSnapshot): void {  
     this.drawMap();
     this.drawBullets(snapshot.bullets);
     this.drawPlayers(snapshot.players);
@@ -42,32 +36,31 @@ export class GameRender {
 
   private drawMap(): void {    
     this.context.fillStyle = 'white';
-    this.context.fillRect(-this.canvas.width / 2, -this.canvas.height / 2, this.canvas.width, this.canvas.height)
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
   }
 
   private drawBullets(bulets: Bullet[]): void {
     bulets.forEach(bullet => {
       this.context.fillStyle = 'black';
-      this.context.fillRect(bullet.x / 2, bullet.y / 2, bullet.width, bullet.height)
+      this.context.fillRect(bullet.x - bullet.width / 2, bullet.y - bullet.height / 2, bullet.width, bullet.height)
     })
   }
   
   private drawPlayers(players: Player[]): void {
     players.forEach(player => {
       this.context.fillStyle ='green';
-      this.context.fillRect(player.x / 2, player.y / 2, player.width, player.height)
+      this.context.fillRect(player.x - player.width / 2, player.y - player.height / 2, player.width, player.height)
     });
   }
 
   private drawEnemies(enemys: Entity[]): void {
     enemys.forEach(enemy => {
       this.context.fillStyle ='red';
-      this.context.fillRect(enemy.x / 2, enemy.y / 2, enemy.width, enemy.height)
+      this.context.fillRect(enemy.x - enemy.width / 2, enemy.y - enemy.height / 2, enemy.width, enemy.height)
     });
   }
 
   private drawParticles(): void {
-    
   }
 
 }
